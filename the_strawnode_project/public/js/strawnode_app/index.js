77aff4aba785acd009a5fa470034cd035a9b007c
sazaam <sazaam@gmail.com>
2015-07-06 19:38:04 +0900

    fixes in filename and dirname of required modules

--- the_strawnode_project/public/js/strawnode_app/index.js
+++ the_strawnode_project/public/js/strawnode_app/index.js
@@ -1,46 +1,31 @@
+// necessary for full app feature
+var express = require('Express') ;
+var routes = require('./routes') ;
 
-(function(name, definition){
-	
-	if ('function' === typeof define){ // AMD
-		define(definition) ;
-	} else if ('undefined' !== typeof module && module.exports) { // Node.js
-		module.exports = ('function' === typeof definition) ? definition() : definition ;
-	} else {
-		if(definition !== undefined) this[name] = ('function' === typeof definition) ? definition() : definition ;
-	}
+var app = express() ;
 
-})('app', (function(){
-
-	// necessary for full app feature
-	var express = require('Express') ;
-	var routes = require('./routes') ;
-
-	var app = express() ;
-
-	return app
-		.set('view_engine', 'jade')
-		.set('views', __public_root + '/jade/')
-		.set('address', {
-			home:'',
-			base:'undefined' !== typeof __parameters ? __parameters.base : location.protocol + '//' + location.host + location.pathname,
-			useLocale:true
-		})
-		.listen('load', function(e){
-			app.discard('load', arguments.callee) ;
-			// PAGE LOAD HANDLING
-			
-			// INIT APP-DEEPLINKED STEPS & BEHAVIORS
-			app
-				.createClient()
-				.get('/', routes)
-				.initAddress() ;
-			
-		})
-		.listen('unload', function(e){
-			app.discard('unload',arguments.callee) ;
-			// PAGE UNLOAD HANDLING
-			app = app.destroy() ;
-			// CLEANING FOR BROWSER'S SAKE, WHO KNOWS
-		}) ;
-
-})()) ;
+module.exports = app
+	.set('view_engine', 'jade')
+	.set('views', __public_root + '/jade/')
+	.set('address', {
+		home:'',
+		base:'undefined' !== typeof __parameters ? __parameters.base : location.protocol + '//' + location.host + location.pathname,
+		useLocale:true
+	})
+	.listen('load', function(e){
+		app.discard('load', arguments.callee) ;
+		// PAGE LOAD HANDLING
+		
+		// INIT APP-DEEPLINKED STEPS & BEHAVIORS
+		app
+			.createClient()
+			.get('/', routes)
+			.initAddress() ;
+		
+	})
+	.listen('unload', function(e){
+		app.discard('unload',arguments.callee) ;
+		// PAGE UNLOAD HANDLING
+		app = app.destroy() ;
+		// CLEANING FOR BROWSER'S SAKE, WHO KNOWS
+	}) ;
